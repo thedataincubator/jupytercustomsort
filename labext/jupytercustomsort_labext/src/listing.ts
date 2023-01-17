39fa47273e022076d7a8ee6c11b3791fe91e93bd
Robert Schroll <rschroll@gmail.com>
2023-01-17 02:20:52 -0800

    Get custom sorting order from server
    This has the base URL hard-coded right now.

--- labext/jupytercustomsort_labext/src/listing.ts
+++ labext/jupytercustomsort_labext/src/listing.ts
@@ -8,32 +8,82 @@ import {
   toArray
 } from '@lumino/algorithm';
 
+class HttpError extends Error {
+  constructor(msg: string, code: number) {
+    super(msg);
+    this.code = code;
+    Object.setPrototypeOf(this, HttpError.prototype);
+  }
+
+  code: number;
+}
+
 export class CustomDirListing extends DirListing {
+  constructor(options: DirListing.IOptions) {
+    super(options);
+    this._model.pathChanged.connect(this.pathChanged, this);
+    this.pathChanged();
+  }
+
   sort(state: DirListing.ISortState): void {
-    console.log("Look, ma, I'm sorting")
     // Access private attributes
-    this['_sortedItems'] = Private.sort(this.model.items(), state);
+    this['_sortedItems'] = Private.sort(this.model.items(), state, this.customOrder);
     this['_sortState'] = state;
     this.update();
   }
+
+  async pathChanged(): Promise<void> {
+    this.customOrder = [];
+    let base_url = '/';
+    let order_url = `${base_url}customorder/${this._model.path}`;
+    try {
+      let response = await fetch(order_url);
+      if (!response.ok)
+        throw new HttpError('Failed to load custom order', response.status);
+      this.customOrder = (await response.json()).order;
+      this.sort(this['_sortState']);
+    } catch (error) {
+      if (!(error instanceof HttpError) || error.code !== 404)
+        console.log(error);
+    }
+  }
+
+  protected customOrder: Array<string> = [];
 }
 
 namespace Private {
   namespace CmpFunctions {
-    export function last_modified(a: Contents.IModel, b: Contents.IModel) {
+    export function last_modified(
+      a: Contents.IModel,
+      b: Contents.IModel,
+      _: Array<string>
+    ): number {
       const valA = new Date(a.last_modified).getTime();
       const valB = new Date(b.last_modified).getTime();
       return valA - valB;
     }
 
-    export function name(a: Contents.IModel, b: Contents.IModel) {
+    export function name(
+      a: Contents.IModel,
+      b: Contents.IModel,
+      customOrder: Array<string>
+    ): number {
+      const aIndex = customOrder.indexOf(a.name);
+      const bIndex = customOrder.indexOf(b.name);
+      if (aIndex > -1 && bIndex > -1)
+        return bIndex - aIndex;
+      if (aIndex > -1)
+        return 1;
+      if (bIndex > -1)
+        return -1;
       return b.name.localeCompare(a.name);
     }
   };
 
   export function sort(
     items: IIterator<Contents.IModel>,
-    state: DirListing.ISortState
+    state: DirListing.ISortState,
+    customOrder: Array<string>
   ): Contents.IModel[] {
     const copy = toArray(items);
     const reverse = state.direction === 'descending' ? 1 : -1;
@@ -44,7 +94,7 @@ namespace Private {
       const t1 = a.type === 'directory' ? 0 : 1;
       const t2 = b.type === 'directory' ? 0 : 1;
 
-      return t1 - t2 || cmpFunc(a, b) * reverse;
+      return t1 - t2 || cmpFunc(a, b, customOrder) * reverse;
     });
     return copy;
   }
